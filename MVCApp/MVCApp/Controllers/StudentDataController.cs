baff16a26749c77931d1365a5d8f387369e3167d
KAILASH <kailashsolanki08@gmail.com>
2026-09-12 10:41:32 +0530

    added cookies session

--- MVCApp/MVCApp/Controllers/StudentDataController.cs
+++ MVCApp/MVCApp/Controllers/StudentDataController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using MVCApp.DAL;
@@ -68,14 +69,36 @@ namespace MVCApp.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        public JsonResult GetUserPassword() {
+            HttpCookie httpCookie = Request.Cookies["UserCookieTemp1"];
+            if (httpCookie != null)
+            {
+                var uname = httpCookie.Values["Username"].ToString();
+                var pass = httpCookie.Values["Password"].ToString();
+                return Json(new { success = true, password = pass }, JsonRequestBehavior.AllowGet);
+            }
+            return null;
+        }
+       
         [HttpPost]
         public ActionResult Login(UserLogin userLogin)
         {
             bool isValidUser = dbOperation.Login(userLogin);
             if (isValidUser)
             {
-               Session["UserName"] = userLogin.UserName;
+               
+                Session["UserName"] = userLogin.UserName;
                 Session.Timeout= 30; // Set session timeout to 30 minutes
+                //create a cookie to store the username
+                HttpCookie cookie = new HttpCookie("UserCookieTemp1");
+                cookie["Username"] = userLogin.UserName;
+                cookie["Password"]=userLogin.Password;
+                //cookie.Expires = DateTime.Now.AddDays(30); 
+               // cookie.HttpOnly = true; 
+                //cookie.Secure = Request.IsSecureConnection; 
+                Response.Cookies.Add(cookie);
                 return RedirectToAction("Index", "StudentData");
             }
             else
